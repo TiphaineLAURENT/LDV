644fa05ddd80056422dbfb0a28f9cb771fe72e54
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-05-25 16:30:37 +0200

    Add media urls conditionnally to Debug

--- ldv/ldv/urls.py
+++ ldv/ldv/urls.py
@@ -23,4 +23,7 @@ import shopping.urls as shopping
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("shopping/", include(shopping)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
