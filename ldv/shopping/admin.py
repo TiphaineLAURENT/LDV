b87dd0975d2b1a5816a22c69427d298a7514a893
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-05-26 15:06:27 +0200

    Split Vetement into Pantalon and Robe

--- ldv/shopping/admin.py
+++ ldv/shopping/admin.py
@@ -14,16 +14,16 @@ class UserAdmin(admin.ModelAdmin):
     filter_horizontal = ('user_permissions', 'groups')
     inlines = (ItemInline,)
 
-@admin.register(Vetement)
-class VetementAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'price', 'description_trunc')
-    ordering = ('name', 'price')
-    search_fields = ('name', 'price')
-    fieldsets = (
-        ("Text", {'fields': ('name', 'description')}),
-        ("Number", {'fields': ('price',)}),
-        (None, {'fields': ('image',)}),
-    )
+#@admin.register(Vetement)
+#class VetementAdmin(admin.ModelAdmin):
+#    list_display = ('id', 'name', 'description', 'price', 'description_trunc')
+#    ordering = ('name', 'price')
+#    search_fields = ('name', 'price')
+#    fieldsets = (
+#        ("Text", {'fields': ('name', 'description')}),
+#        ("Number", {'fields': ('price',)}),
+#        (None, {'fields': ('image',)}),
+#    )
 
 models = apps.get_models()
 
--- ldv/shopping/models.py
+++ ldv/shopping/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
+from django.core.files.images import ImageFile
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+import os
+
+from polymorphic.models import PolymorphicModel
+
 
 # Create your models here.
 
@@ -21,15 +27,14 @@ class Item(models.Model):
         return f"{self.vetement.name} for {self.user.username}"
 
 
-class Vetement(models.Model):
+class Vetement(PolymorphicModel):
     """
      Table représentant des vetements
     """
 
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    description = models.TextField(help_text="Une courte description")
     price = models.FloatField(default=50)
-    image = models.ImageField(null=True, default="/media/robe.jpg")
 
     def __str__(self):
         return self.name
@@ -40,3 +45,30 @@ class Vetement(models.Model):
     class Meta:
         verbose_name = "Vetement"
         verbose_name_plural = "Vetements"
+
+
+class Robe(Vetement):
+    """
+    """
+
+    image = models.ImageField(default="/robe.jpg", upload_to="", blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.name.lower().startswith("robe"):
+            self.name = f"Robe {self.name}"
+        return super().save(*args, **kwargs)
+
+
+class Pantalon(Vetement):
+    """
+    """
+
+    image = models.ImageField(default="/pantalon.jpg", upload_to="", blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.name.lower().startswith("pantalon"):
+            self.name = f"Pantalon {self.name}"
+        if not self.image:
+            print(os.getcwd())
+            self.image = self.image.field.default
+        return super().save(*args, **kwargs)
